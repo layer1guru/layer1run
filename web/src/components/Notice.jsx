2cce2941f0df10ccba6b1ed9aaaf6a8b3be7ef52
Shomari <nyusternie@sdot.io>
2025-04-15 03:44:18 -0400

    Update voting link.

--- web/src/components/Notice.jsx
+++ web/src/components/Notice.jsx
@@ -143,7 +143,7 @@ export function NodeRunr() {
 
                         <div className="mt-3">
                             <Link
-                                href="https://retro9000.avax.network"
+                                href="https://retro9000.avax.network/discover-builders/cm490lkn9004bbuff1hmdkj8r"
                                 target="_blank"
                                 className="inline-flex w-full items-center justify-center rounded-md bg-sky-600 px-3 py-2 text-2xl font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
                             >
